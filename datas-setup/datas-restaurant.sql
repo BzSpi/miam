c634379d2211337bb95011838de6f89059c1d58d
Baïzé Spi <spirouspy@hotmail.com>
2013-02-24 15:24:53 +0100

    Adding Mysql restaurant datas file

---
+++ datas-setup/datas-restaurant.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+--
+-- Restaurant datas for table `place_type`
+--
+INSERT INTO `place_type` (`label`, `name`) VALUES
+('african', 'african'),
+('barbecue', 'barbecue'),
+('brasserie', 'brasserie'),
+('breakfast', 'breakfast'),
+('buffet', 'buffet'),
+('cafetaria', 'cafetaria'),
+('chinese', 'chinese'),
+('fastfood', 'fastfood'),
+('fish', 'fish'),
+('fishchips', 'fishchips'),
+('gourmet', 'gourmet'),
+('indian', 'indian'),
+('italian', 'italian'),
+('japanese', 'japanese'),
+('kebab', 'kebab'),
+('korean', 'korean'),
+('mediterranean', 'mediterranean'),
+('mexican', 'mexican'),
+('pizzeria', 'pizzeria'),
+('restaurant', 'restaurant'),
+('vegetarian', 'vegetarian'),
+('romantic', 'romantic'),
+('steakhouse', 'steakhouse'),
+('tapas', 'tapas'),
+('terrace', 'terrace'),
+('thai', 'thai'),
+('turkish', 'turkish');
