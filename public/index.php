79abf720667f34fc247f5da901f68842c4877a7c
BzSpi <laurent.piroelle@live.fr>
2013-03-16 16:25:15 +0100

    Skin improvement on popover

--- public/index.php
+++ public/index.php
@@ -29,22 +29,20 @@ $mapConfig  = $config['map'];
     <div id="navbar" class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container-fluid">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <!--a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </a>
+          </a-->
           <a class="brand" href="#">Miam!</a>
-          <div class="nav-collapse collapse">
-            <p class="navbar-text pull-right">
-              <input type="button" data-toggle-button="addMode" class="btn btn-primary" value="Add a new place" data-popover-placement="left" data-popover-content="Click on the map to add a new place" />
-            </p>
+          <p class="navbar-text pull-right">
+            <input type="button" data-toggle-button="addMode" class="btn btn-primary" value="Add a new place" data-popover-placement="left" data-popover-content="Click on the map to add a new place" />
+          </p>
             <!--ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul-->
-          </div><!--/.nav-collapse -->
         </div>
       </div>
     </div>
